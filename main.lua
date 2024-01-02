0adc061d135f85bb6ac4cd20b43ea4cb78db8af2
Vladislav Naumov <vnaum@vnaum.com>
2024-01-02 16:51:00 +0600

    multicolored sprites

--- main.lua
+++ main.lua
@@ -33,6 +33,7 @@ function love.load()
   player.sx = 0
   player.sy = 0
   player.dir = 1
+  player.color = { love.math.colorFromBytes(255, 10, 10) }
   player.control = { left="a", right="d", up = "w", fire = "space" }
 
   table.insert(players, player)
@@ -43,6 +44,7 @@ function love.load()
   player.sx = 0
   player.sy = 0
   player.dir = -1
+  player.color = { love.math.colorFromBytes(10, 10, 255) }
   player.control = { left="left", right="right", up = "up", fire = "rctrl" }
 
   table.insert(players, player)
@@ -85,6 +87,9 @@ function love.draw()
   -- love.graphics.print('hgt: '.. love.graphics.getHeight())
 
   for i, player in ipairs(players) do
+    love.graphics.push()
+    love.graphics.setColor(player.color)
     love.graphics.draw(chopperimg, player.x, player.y, 0, player.dir, 1, choppermid, choppermid)
+    love.graphics.pop()
   end
 end
