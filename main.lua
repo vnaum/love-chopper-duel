f46e3dab50b8d753393f08223176808a182cd821
Vladislav Naumov <vnaum@vnaum.com>
2024-01-02 11:56:00 +0600

    control1

--- main.lua
+++ main.lua
@@ -8,19 +8,31 @@ local function inverseLerp(a, b, x)
 end
 
 player = {}
+grav = 5 -- p/s/s
+termvelx = 50
+termvely = 100
 
 function love.load()
   chopperimg = love.graphics.newImage("chopper.png")
+  choppermid = chopperimg:getWidth() / 2
 
   player.x = 200
   player.y = 100
+  player.dir = 1
+  player.control = { left="a", right="d", up = "w" }
 end
 
 function love.update(dt)
+  if love.keyboard.isDown(player.control.left) then
+    player.dir = -1
+  end
+  if love.keyboard.isDown(player.control.right) then
+    player.dir = 1
+  end
 end
 
 function love.draw()
   love.graphics.print('hgt: '.. love.graphics.getHeight())
 
-  love.graphics.draw(chopperimg, player.x, player.y)
+  love.graphics.draw(chopperimg, player.x, player.y, 0, player.dir, 1, choppermid, choppermid)
 end
