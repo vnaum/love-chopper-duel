44cbdbc1028f271177cb6c00564c3da8ff370d2b
Vladislav Naumov <vnaum@vnaum.com>
2024-01-02 16:37:22 +0600

    multiplayer

--- main.lua
+++ main.lua
@@ -17,7 +17,7 @@ function limit(val, absmax)
   end
 end
 
-player = {}
+players = {}
 grav = 5 -- p/s/s
 accelx = 5 -- p/s/s
 termvelx = 50
@@ -27,46 +27,64 @@ function love.load()
   chopperimg = love.graphics.newImage("chopper.png")
   choppermid = chopperimg:getWidth() / 2
 
+  local player = {}
   player.x = 200
   player.y = 100
   player.sx = 0
   player.sy = 0
   player.dir = 1
   player.control = { left="a", right="d", up = "w", fire = "space" }
+
+  table.insert(players, player)
+
+  local player = {}
+  player.x = 600
+  player.y = 200
+  player.sx = 0
+  player.sy = 0
+  player.dir = -1
+  player.control = { left="left", right="right", up = "up", fire = "rctrl" }
+
+  table.insert(players, player)
 end
 
 function love.update(dt)
-  if love.keyboard.isDown(player.control.left) then
-    player.sx = limit(player.sx - accelx * dt, termvelx)
-    player.dir = -1
-  end
 
-  if love.keyboard.isDown(player.control.right) then
-    player.sx = limit(player.sx + accelx * dt, termvelx)
-    player.dir = 1
-  end
+  for i, player in ipairs(players) do
+    if love.keyboard.isDown(player.control.left) then
+      player.sx = limit(player.sx - accelx * dt, termvelx)
+      player.dir = -1
+    end
 
-  if love.keyboard.isDown(player.control.up) then
-    player.sy = limit(player.sy - accelx * dt, termvely)
-  else
-    player.sy = limit(player.sy + grav * dt, termvely)
-  end
+    if love.keyboard.isDown(player.control.right) then
+      player.sx = limit(player.sx + accelx * dt, termvelx)
+      player.dir = 1
+    end
 
-  player.y = player.y + player.sy
-  player.x = player.x + player.sx
+    if love.keyboard.isDown(player.control.up) then
+      player.sy = limit(player.sy - accelx * dt, termvely)
+    else
+      player.sy = limit(player.sy + grav * dt, termvely)
+    end
 
-  -- screen boundaries
-  if player.y < 0 or player.y > love.graphics.getHeight() then
-    player.sy = -player.sy
-  end
+    player.y = player.y + player.sy
+    player.x = player.x + player.sx
 
-  if player.x < 0 or player.x > love.graphics.getWidth() then
-    player.sx = -player.sx
+    -- screen boundaries
+    if player.y < 0 or player.y > love.graphics.getHeight() then
+      player.sy = -player.sy
+    end
+
+    if player.x < 0 or player.x > love.graphics.getWidth() then
+      player.sx = -player.sx
+    end
   end
 end
 
 function love.draw()
   -- love.graphics.print('hgt: '.. love.graphics.getHeight())
 
-  love.graphics.draw(chopperimg, player.x, player.y, 0, player.dir, 1, choppermid, choppermid)
+  for i, player in ipairs(players) do
+    love.graphics.draw(chopperimg, player.x, player.y, 0, player.dir, 1, choppermid, choppermid)
+  end
 end
